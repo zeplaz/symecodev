99743647bf1b9a61bcc477901cad2091f55e4106
zeplaz <life.in.the.vivid.dream@gmail.com>
2018-11-04 04:05:07 -0500

    sysmdata0.3updatenov3

--- test3_railhubs/trainz.h
+++ test3_railhubs/trainz.h
@@ -8,29 +8,48 @@ class Tranz : //public entity
 
 {
 private :
-railhubz* Orign_Station;
-railhubz* Destination_station;
+    int* Train_id;
+    int speed;
+    sf::Vector2f* current_location;
 
-railhubz* current_hub =Null;
-R_linez* current_Rail_Line = null;
+    railhubz* Orign_Station;
+    railhubz* Destination_station;
 
-sf::Time lanchtime_orgin;
-sf::Time timesince_lastHub;
+    sf::Time lanchtime_orgin;
+    sf::Time timesince_lastHub;
 
-int* Train_id;
-int speed;
+//
+struct train_data{
+    bool is_inStation;
 
-int* distance_traveld_total;
+    int distance_traveld_total;
+    int numberofCarts;
 
-int resorse1;
-int resorse2;
-int resorse3;
+        struct cargo{
+            int resorse1;
+            int resorse2;
+            int resorse3;
 
-bool is_inStation;
+            }cargo;
+    };
+
+//grpahics
+enum cagrotype_colour
+{ red_res1, magenta_res2, green_res3,
+}
 
 sf::Shape Triangle_train_engine;
 Triangle_train_engine.SetPointColor(0, sf::Color::Blue);
 Triangle_train_engine.SetPointOutlineColor(0, sf::Color(0, 128, 128));
+stack<RectangleShape*>  cargoz;
+for (int i = 0; i <=cargo.numberofCarts; i++ )
+    { cargoz.push(sf::RectangleShape* i = new sf::RectangleShape);
+        cargoz.top()-> setColour();
+
+    }
+
+
+
 
 
 public :

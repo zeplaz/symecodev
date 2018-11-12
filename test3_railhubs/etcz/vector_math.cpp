f7fcfc04744fdf77a6527c632a6284f189400507
zeplaz <life.in.the.vivid.dream@gmail.com>
2018-11-12 01:00:46 -0500

    sysmdata0.10.2updatenov11.no_compilrvalidtestrailhubz0.0.30

--- test3_railhubs/etcz/vector_math.cpp
+++ test3_railhubs/etcz/vector_math.cpp
@@ -1,7 +1,7 @@
 
 //vector_math.cpp
 
-#include "etcz/Vector_math"
+#include "etcz/Vector_math.h"
 
 
 float vector_math::dot (const vector2d& vec1, const vector2d& vec2);
@@ -9,4 +9,20 @@ float vector_math::dot (const vector2d& vec1, const vector2d& vec2);
 {
 return vec1.x*vec2.x + vec1.y*vec2.y;
 
+}
+
+void vector_math::rotate (Vec2& res, const Vec2& v, float angle);
+{
+    const float rad = DEG2RAD * angle;
+    const float sin0 = sin(rad);
+    const float cos0 = cos(rad);
+    res.x = v.x * cos0 + v.y * -sin0;
+    res.y = v.x * sin0 + v.y * cos0;
+}
+
+vector2d vector_math::rotate (Vec2& v, float angle);
+{
+   Vec2 res;
+   rotate(res, v, angle);
+   return res;
 }
